7daa7a91417224cdf18d1d0852d19d7cd9e8ba78
Filip Luśnia <uszaty223@onet.pl>
2020-05-04 11:43:14 +0200

    cleanup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/App.scss';
+
+function App() {
+  const x = 5;
+  return (
+    <div className="App">
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
