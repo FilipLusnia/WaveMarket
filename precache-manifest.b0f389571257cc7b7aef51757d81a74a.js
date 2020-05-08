e15eca5dceae503c1aecb4dc4cdde30cecccff4c
Filip Luśnia <uszaty223@onet.pl>
2020-05-08 14:37:33 +0200

    Updates

---
+++ precache-manifest.b0f389571257cc7b7aef51757d81a74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9efd55b04a6c7a11ffec8b95f0d1ed5",
+    "url": "/WaveMarket/index.html"
+  },
+  {
+    "revision": "ac55bc64bb409ae96fee",
+    "url": "/WaveMarket/static/css/main.dfe93bad.chunk.css"
+  },
+  {
+    "revision": "08e830a46e2792e91ff7",
+    "url": "/WaveMarket/static/js/2.52bd861b.chunk.js"
+  },
+  {
+    "revision": "91dbce2febfc227edf628f0fd0b15354",
+    "url": "/WaveMarket/static/js/2.52bd861b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac55bc64bb409ae96fee",
+    "url": "/WaveMarket/static/js/main.ee596437.chunk.js"
+  },
+  {
+    "revision": "68d68b0a631a6118bf6c",
+    "url": "/WaveMarket/static/js/runtime-main.9ac37cdc.js"
+  }
+]);
